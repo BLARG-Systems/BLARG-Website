3eafc4464406048abb143422e5a61ca6aec9fe8a
Computercodes101 <computercodes101@gmail.com>
2023-10-25 16:13:37 -0600

    More Templating

--- docs/assets/template.js
+++ docs/assets/template.js
@@ -1,11 +1,9 @@
 getHead() {
     document.write(`
-        <head>
-            <link rel="stylesheet" href="style.css" />
-            <link rel="icon" href="assets/favicon.ico" />
-            <meta charset="utf-8" />
-            <title>CalcVox - Goals</title>
-        </head>
+        <link rel="stylesheet" href="style.css" />
+        <link rel="icon" href="assets/favicon.ico" />
+        <meta charset="utf-8" />
+        <Title>BLARG! Systems</Title>
     `)
 }
 
@@ -14,13 +12,13 @@ getHeader() {
 		<header>
             <div class="titlebar">
                 <a href="/" aria-hidden="true"><img src="assets/logo.png" /></a>
-                <h1>CalcVox: <i>Bridging The Gap in Mathematics</i></h1>
+                <h1>BLARG! Systems: <i>Approaching expectations.</i></h1>
             </div>
             <nav>
                 <ul class="navbar">
                     <li><a href="/">Home</a></li>
-                    <li><a href="advisors.html">Our Advisors</a></li>
-                    <li><a href="goals.html">Our Goals</a></li>
+                    <li><a href="advisors.html">Advisor Committee</a></li>
+                    <li><a href="objectives.html">Weekly Objectives</a></li>
                     <li><a href="journals.html">Journals and Research Papers</a></li>
                     <li><a href="timeline.html">Project Timeline</a></li>
                 </ul>
